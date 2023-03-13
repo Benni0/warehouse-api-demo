79d91b9d7d5e4377ef36a31b32476d09605e918a
Benni0 <me@benji.at>
2023-03-13 12:09:41 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,4 +6,6 @@ class Article(Base):
     article_number = Column(Integer, primary_key=True, unique=True, index=True, autoincrement=False)
     article_name = Column(String(60), index=True)
     items_available = Column(Integer)
-
+    
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
